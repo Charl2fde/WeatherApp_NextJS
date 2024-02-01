5f92d93b7486ce762de9ba378528d68060e2dd67
Charles2fde <99027192+Charl2fde@users.noreply.github.com>
2024-02-01 16:09:22 +0100

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -14,7 +14,7 @@ import {
 import { Card, CardBody } from '@chakra-ui/react';
 import CurrentWeatherCard from '../components/cardToday';
 
-const API_KEY = '$API_KEY';
+const API_KEY = '6b1d5ecb1b816eb86b1b035afb017936';
 
 export default function Home() {
   const [city, setCity] = useState('');
